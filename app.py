b1f8382c38cdb3743409f6ec02afc87995b40170
Cole Nollette <155999592+nollettecs@users.noreply.github.com>
2024-04-08 11:32:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,59 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
+
+# From shiny, import just reactive and render
+from shiny import reactive, render
+
+# From shiny.express, import just ui and inputs if needed
+from shiny.express import ui
+
+# --------------------------------------------
+# Import icons as you like
+# --------------------------------------------
+
+# Theme of Shiny
+shinyswatch.theme.darkly()
+
+# --------------------------------------------
+# Shiny EXPRESS VERSION
+# --------------------------------------------
+
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+
+# --------------------------------------------
+# Initialize a REACTIVE VALUE with a common data structure
+# The reactive value is used to store state (information)
+# Used by all the display components that show this live data.
+# This reactive value is a wrapper around a DEQUE of readings
+# --------------------------------------------
+
+# --------------------------------------------
+# Initialize a REACTIVE CALC that all display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+# It returns a tuple with everything needed to display the data.
+# Very easy to expand or modify.
+# --------------------------------------------
+
+# Define the Shiny UI Page layout
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+
+# In Shiny Express, everything not in the sidebar is in the main panel
+
+# Linear regression - we need to get a list of the
+            # Independent variable x values (time) and the
+            # Dependent variable y values (temp)
+            # then, it's pretty easy using scipy.stats.linregress()
 
